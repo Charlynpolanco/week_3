dd5f8b77d41171c127592923cf61873edc6011d9
Charlynpolanco <110350236+Charlynpolanco@users.noreply.github.com>
2022-08-25 10:08:30 -0400

    Add files via upload
    3 css hw

---
+++ hw_26/app.js
@@ -0,0 +1,17 @@
+const buttonInfo = document.querySelector(".nextbtn");
+
+
+ buttonInfo.addEventListener('click', function(){ 
+        alert("Next movie is Gremlins")
+})
+   
+
+const buttonLast = document.querySelector(".lastbtn");
+
+    buttonLast.addEventListener('click', function(){
+    alert("Last movies was Superbad")
+})
+
+
+
+
